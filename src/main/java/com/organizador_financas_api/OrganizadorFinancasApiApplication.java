cd2e2f7838a98bcc25e988c742afa1a75993d907
rubensrochabs <rubensrochabs@gmail.com>
2024-06-06 15:46:12 -0300

    commit inicial

---
+++ src/main/java/com/organizador_financas_api/OrganizadorFinancasApiApplication.java
@@ -0,0 +1,13 @@
+package com.organizador_financas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorFinancasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorFinancasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/organizador_financas_api/OrganizadorFinancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organizador_financas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorFinancasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
