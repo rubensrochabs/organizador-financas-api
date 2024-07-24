880d6888cfc2b9f5021acdac14561fd018ab0240
rubensrochabs <rubensrochabs@gmail.com>
2024-07-23 22:13:17 -0300

    refact: retirando lombok de PessoaDto

--- src/main/java/com/organizador_financas_api/model/dto/PessoaDto.java
+++ src/main/java/com/organizador_financas_api/model/dto/PessoaDto.java
@@ -1,10 +1,32 @@
 package com.organizador_financas_api.model.dto;
 
-import lombok.Data;
-
-@Data
 public class PessoaDto {
 
 	private Long id;
 	private String nmNome;
+
+	public PessoaDto() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNmNome() {
+		return nmNome;
+	}
+
+	public void setNmNome(String nmNome) {
+		this.nmNome = nmNome;
+	}
+
+	@Override
+	public String toString() {
+		return "PessoaDto [id=" + id + ", nmNome=" + nmNome + "]";
+	}
 }
