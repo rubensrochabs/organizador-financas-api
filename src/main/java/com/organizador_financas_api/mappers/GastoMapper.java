72ce5dce2d9adec34afe64307f1a1e3c8bf368c3
rubensrochabs <rubensrochabs@gmail.com>
2024-07-23 22:22:47 -0300

    feat: adicionando mapear GastoSaidaDto

--- src/main/java/com/organizador_financas_api/mappers/GastoMapper.java
+++ src/main/java/com/organizador_financas_api/mappers/GastoMapper.java
@@ -2,13 +2,15 @@ package com.organizador_financas_api.mappers;
 
 import org.springframework.stereotype.Component;
 
-import com.organizador_financas_api.model.dto.GastoDto;
+import com.organizador_financas_api.model.dto.GastoEntradaDto;
+import com.organizador_financas_api.model.dto.GastoSaidaDto;
 import com.organizador_financas_api.model.entity.Gasto;
+import com.organizador_financas_api.model.enums.GastoCategoriaEnum;
 
 @Component
 public class GastoMapper {
 
-	public Gasto mapear(final GastoDto dto) {
+	public Gasto mapear(final GastoEntradaDto dto) {
 		Gasto entidade = new Gasto();
 
 		entidade.setIdGasto(dto.getIdGasto());
@@ -21,12 +23,27 @@ public class GastoMapper {
 		return entidade;
 	}
 
-	public GastoDto mapear(final Gasto entidade) {
-		GastoDto dto = new GastoDto();
+	public GastoEntradaDto mapear(final Gasto entidade) {
+		GastoEntradaDto dto = new GastoEntradaDto();
 
 		dto.setIdGasto(entidade.getIdGasto());
 		dto.setIdPessoa(entidade.getIdPessoa());
 		dto.setIdGastoCategoria(entidade.getIdGastoCategoria());
+		dto.setDtEmissao(entidade.getDtEmissao());
+		dto.setNrValor(entidade.getNrValor());
+		dto.setTxDescricao(entidade.getTxDescricao());
+
+		return dto;
+	}
+
+	public GastoSaidaDto mapear(final Gasto entidade, final String nmPessoa) {
+		GastoSaidaDto dto = new GastoSaidaDto();
+
+		dto.setIdGasto(entidade.getIdGasto());
+		dto.setIdPessoa(entidade.getIdPessoa());
+		dto.setNmPessoa(nmPessoa);
+		dto.setIdGastoCategoria(entidade.getIdGastoCategoria());
+		dto.setNmGastoCategoria(GastoCategoriaEnum.getByCodigo(entidade.getIdGastoCategoria()).getNmNome());
 		dto.setDtEmissao(entidade.getDtEmissao());
 		dto.setNrValor(entidade.getNrValor());
 		dto.setTxDescricao(entidade.getTxDescricao());
