887423fe7b4f4c6e2f53bdba063af97cb36c6c65
rubensrochabs <rubensrochabs@gmail.com>
2024-07-23 22:25:17 -0300

    refact: refatorando endpoints para usar novos DTOs

--- src/main/java/com/organizador_financas_api/controllers/GastoController.java
+++ src/main/java/com/organizador_financas_api/controllers/GastoController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.organizador_financas_api.model.dto.GastoDto;
+import com.organizador_financas_api.model.dto.GastoEntradaDto;
+import com.organizador_financas_api.model.dto.GastoSaidaDto;
 import com.organizador_financas_api.model.dto.StandardResponse;
 import com.organizador_financas_api.services.GastoService;
 import com.organizador_financas_api.utils.StandardController;
@@ -24,21 +25,22 @@ public class GastoController implements StandardController {
 	private GastoService gastoService;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<StandardResponse<GastoDto>> buscarGastoById(@PathVariable final Long id) {
-		GastoDto gastoDto = gastoService.buscarPorId(id);
-		return retornarSucesso(gastoDto);
+	public ResponseEntity<StandardResponse<GastoSaidaDto>> buscarGastoById(@PathVariable final Long id) {
+		GastoSaidaDto gastoSaidaDto = gastoService.buscarPorId(id);
+		return retornarSucesso(gastoSaidaDto);
+	}
 	}
 
 	@PostMapping
-	public ResponseEntity<StandardResponse<GastoDto>> incluirGasto(@RequestBody final GastoDto gastoDto) {
-		GastoDto retornoGastoDto = gastoService.incluir(gastoDto);
+	public ResponseEntity<StandardResponse<GastoEntradaDto>> incluirGasto(@RequestBody final GastoEntradaDto gastoDto) {
+		GastoEntradaDto retornoGastoDto = gastoService.incluir(gastoDto);
 		return retornarCriado(retornoGastoDto);
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<StandardResponse<GastoDto>> atualizarGasto(@PathVariable final Long id,
-			@RequestBody final GastoDto gastoDto) {
-		GastoDto retornoGastoDto = gastoService.alterar(id, gastoDto);
+	public ResponseEntity<StandardResponse<GastoEntradaDto>> atualizarGasto(@PathVariable final Long id,
+			@RequestBody final GastoEntradaDto gastoDto) {
+		GastoEntradaDto retornoGastoDto = gastoService.alterar(id, gastoDto);
 		return retornarSucesso(retornoGastoDto);
 	}
 
