4c17bc11e41c6cc6d518eedc06a0d817df89ede5
rubensrochabs <rubensrochabs@gmail.com>
2024-07-30 21:30:31 -0300

    refact: organizando imports

--- src/main/java/com/organizador_financas_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/organizador_financas_api/exception/GlobalExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.organizador_financas_api.exception;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
